5dd1fe51d3cf14a352388bf5a6c864d5d2612461
Atoshi <atoshit@protonmail.com>
2025-01-12 19:51:53 +0100

    Refactor core initialization and utility functions: rename Init to Initialize, add IsType and IsResourceStarted functions, and enhance Debug function for improved logging and clarity. Update core.utils to include new utility functions for better maintainability.

---
+++ shared/class.lua
@@ -0,0 +1,182 @@
+local mt_private = {
+    __metatable = 'private',
+    __ext = 0,
+    __pack = function() return '' end,
+}
+
+---@class ClassOptions
+---@field private? table Table de données privées
+---@field export? string Nom d'export de la classe
+---@field init? function Fonction d'initialisation
+
+---@param obj ClassOptions
+---@return table
+local function NewInstance(self, obj)
+    if obj.private then
+        setmetatable(obj.private, mt_private)
+    end
+
+    setmetatable(obj, self)
+
+    if self.init then 
+        obj:init() 
+    end
+
+    if obj.export then
+        self.__exports[obj.export] = obj
+    end
+
+    return obj
+end
+
+---@class ClassDefinition
+---@field __name string Nom de la classe
+---@field __parent? table Classe parente
+---@field __exports table<string, table> Table des instances exportées
+---@field __exportMethods table<string, boolean> Table des méthodes exportées
+---@field new function Fonction de création d'instance
+
+---@param name string Nom de la classe
+---@param parent? table Classe parente
+---@param exportable? boolean Permet l'export des méthodes
+---@return ClassDefinition
+function core.class(name, parent, exportable)
+    if not name then 
+        core.utils.Debug('ERROR', 'Class name is required')
+        return 
+    end
+
+    local class = {
+        __name = name,
+        __parent = parent,
+        __exports = {},
+        __exportMethods = {},
+        new = NewInstance,
+    }
+
+    class.__index = class
+
+    if parent or exportable then
+        if exportable then
+            setmetatable(class, {
+                __index = parent,
+                __newindex = function(self, key, value)
+                    rawset(self, key, value)
+                    if type(value) == 'function' then
+                        self.__exportMethods[key] = true
+                    end
+                end
+            })
+
+            core[name] = function(method, ...)
+                local export = class.__exports[name]
+                if export and export[method] then
+                    return export[method](export, ...)
+                end
+                core.utils.Debug('ERROR', ('Invalid export call: %s.%s'):format(name, method or 'nil'))
+                return nil
+            end
+        elseif parent then
+            setmetatable(class, {
+                __index = parent
+            })
+        end
+    end
+
+    return class
+end
+
+-- Exemples d'utilisation
+--[[
+-- 1. Classe simple
+local Animal = core.class('Animal')
+
+function Animal:init()
+    self.alive = true
+end
+
+function Animal:makeSound()
+    return 'Some sound'
+end
+
+local dog = Animal:new({
+    name = 'Rex',
+    private = {
+        age = 5
+    }
+})
+
+-- 2. Héritage
+local Dog = core.class('Dog', Animal)
+
+function Dog:init()
+    Animal.init(self) -- Appel du constructeur parent
+    self.species = 'dog'
+end
+
+function Dog:makeSound()
+    return 'Woof!'
+end
+
+local myDog = Dog:new({
+    name = 'Max',
+    private = {
+        age = 3
+    }
+})
+
+-- 3. Classe exportable
+local Vehicle = core.class('Vehicle', nil, true)
+
+function Vehicle:init()
+    self.speed = 0
+end
+
+function Vehicle:accelerate(amount)
+    self.speed = self.speed + amount
+    return self.speed
+end
+
+local car = Vehicle:new({
+    export = 'mainVehicle',
+    model = 'Tesla'
+})
+
+-- Utilisation de l'export
+local speed = core.Vehicle('accelerate', 10)
+
+-- 4. Héritage avec export
+local Car = core.class('Car', Vehicle, true)
+
+function Car:init()
+    Vehicle.init(self)
+    self.wheels = 4
+end
+
+function Car:honk()
+    return 'Beep!'
+end
+
+local tesla = Car:new({
+    export = 'tesla',
+    model = 'Model S'
+})
+
+-- 5. Utilisation des données privées
+local Player = core.class('Player')
+
+function Player:init()
+    self.private.health = 100
+    self.private.armor = 0
+end
+
+function Player:damage(amount)
+    self.private.health = math.max(0, self.private.health - amount)
+    return self.private.health
+end
+
+local player = Player:new({
+    private = {},
+    name = 'John'
+})
+]]
