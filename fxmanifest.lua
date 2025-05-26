82d475c9d0412cf58164eecaa8a38af90d0ef376
Atoshi <atoshit@protonmail.com>
2025-05-26 02:58:54 +0200

    feat: initialize project structure with core files
    Add initial project files including fxmanifest, init script, locale modules, and empty template files. This sets up the basic structure for the FiveM framework with version control, localization support, and MySQL integration.

---
+++ fxmanifest.lua
@@ -0,0 +1,22 @@
+fx_version 'cerulean'
+game 'gta5'
+lua54 'yes'
+use_experimental_fxv2_oal 'yes'
+
+author 'Atoshi (discord: atoshic17)'
+description 'At Core is a modular and user-friendly framework designed for creating FiveM servers. With its simple configuration and advanced features, At Core allows server developers to easily set up and manage their servers while offering the flexibility to customize and expand as needed. Perfect for both beginners and experienced users.'
+version '0.0.1'
+repository 'https://github.com/atoshit/at_core'
+
+files {
+    'configs/**/*.lua',
+    'locales/*.lua',
+    'modules/**/**/*.lua'
+}
+
+shared_script 'init.lua'
+
+server_scripts {
+    '@oxmysql/lib/MySQL.lua',
+    'sources/server/*.lua'
+}
