0097f314736f0223cc48120817857199d34a8959
Atoshi <atoshit@protonmail.com>
2025-05-30 05:58:50 +0200

    chore: bump version from 0.0.2 to 0.1.1

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -5,7 +5,7 @@ use_experimental_fxv2_oal 'yes'
 
 author 'Atoshi (discord: atoshic17)'
 description 'At Core is a modular and user-friendly framework designed for creating FiveM servers. With its simple configuration and advanced features, At Core allows server developers to easily set up and manage their servers while offering the flexibility to customize and expand as needed. Perfect for both beginners and experienced users.'
-version '0.0.2'
+version '0.1.1'
 repository 'https://github.com/atoshit/at_core'
 
 files {
