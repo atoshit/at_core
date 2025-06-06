d3162f5f7f2f0f15eb3d1c9858c1ecbc4fd2f502
Quravat <135114632+Quravat@users.noreply.github.com>
2025-06-06 14:13:10 -0400

    (feat) Spanish Translation

---
+++ locales/es.lua
@@ -0,0 +1,6 @@
+local ES <const> = {
+    ['init.success'] = "El núcleo se ha inicializado correctamente, los metadatos ahora son accesibles en todo el recurso.",
+    ['init.failed'] = "El núcleo no se ha inicializado correctamente, no todos los metadatos son accesibles, verifica tu consola F8 para errores y asegúrate de haber configurado todo en el archivo `at_core_settings.cfg`."
+}
+
+return ES
